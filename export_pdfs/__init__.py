6cbd745a364ededd84946dfafe29cb824da96cca
Senn R <sennrobyns@gmail.com>
2023-07-19 13:58:41 +0200

    test

--- export_pdfs/__init__.py
+++ export_pdfs/__init__.py
@@ -1,13 +1,20 @@
-from lib.main import process_single
 import logging
+
 import azure.functions as func
+
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = 'test'
-    print(id)
-    
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
     if name:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
