5513534c436e38190d050b2cb20493c03f376b00
Senn R <sennrobyns@gmail.com>
2023-07-19 11:50:39 +0200

    test

--- code/function_app.py
+++ code/function_app.py
@@ -4,5 +4,14 @@ from _azure.export_pdf.main import export_pdf_bp
 app = func.FunctionApp()
 app.register_functions(export_pdf_bp)
 
+
+@app.function_name('hello')
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    return func.HttpResponse(
+        "Hello, World! This HTTP triggered function executed successfully.",
+        status_code=200
+    )
+
+
 # create new test function
 app.register_functions(func.HttpResponse("Hello, World! This HTTP triggered function executed successfully."))
