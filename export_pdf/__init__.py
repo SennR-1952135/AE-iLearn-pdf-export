7933424dc7fa9a82a0390bf279ad7ce3e29ab803
Senn R <sennrobyns@gmail.com>
2023-07-19 18:03:15 +0200

    test

--- export_pdf/__init__.py
+++ export_pdf/__init__.py
@@ -11,11 +11,8 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     id = get_param(req, 'id')
     logging.info('id: ' + id)
     logging.info('bearer_token: ' + bearer_token)
-    
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+
+    return func.HttpResponse(
+        f"Hello, {name}. id is {id}. bearer_token is {bearer_token}. This HTTP triggered function executed successfully.",
+        status_code=200
+    )
