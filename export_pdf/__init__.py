560a0c525ec2440456de03f951476793212e37d4
Senn R <sennrobyns@gmail.com>
2023-07-19 13:45:42 +0200

    test

--- export_pdf/__init__.py
+++ export_pdf/__init__.py
@@ -2,28 +2,27 @@ from lib.main import process_single
 import logging
 import azure.functions as func
 
-export_pdf_bp = func.Blueprint()
+# def get_param(req: func.HttpRequest, param_name: str):
+#     param = req.params.get(param_name)
+#     if not param:
+#         try:
+#             req_body = req.get_json()
+#         except ValueError:
+#             pass
+#         else:
+#             param = req_body.get(param_name)
+#     return param
 
-def get_param(req: func.HttpRequest, param_name: str):
-    param = req.params.get(param_name)
-    if not param:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            param = req_body.get(param_name)
-    return param
-
-def export_pdf(url: str, bearer_token: str):
-    file_path = process_single(url, bearer_token)
-    return file_path
+# def export_pdf(url: str, bearer_token: str):
+#     file_path = process_single(url, bearer_token)
+#     return file_path
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = get_param(req, 'name')
-    id = get_param(req, 'id')
+    # name = get_param(req, 'name')
+    # id = get_param(req, 'id')
+    name = 'test'
     print(id)
     
     if name:
