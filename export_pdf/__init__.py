a4fd3c699cd167201d810e3bd67064b509aa1044
Senn R <sennrobyns@gmail.com>
2023-07-20 10:07:08 +0200

    update, pdf generation

--- export_pdf/__init__.py
+++ export_pdf/__init__.py
@@ -1,6 +1,6 @@
 import logging
 import azure.functions as func
-from .utils import get_param
+from .utils import get_param, export_pdf
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
@@ -8,12 +8,12 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     bearer_token = req.headers.get('Authorization')
     id = get_param(req, 'id')
-    logging.info('id: ' + id)
-    logging.info('bearer_token: ' + bearer_token)
+    # logging.info('id: ' + id)
+    # logging.info('bearer_token: ' + bearer_token)
 
     if not id:
         return func.HttpResponse(
-            "Please pass a learning-track-id as a parameter on the query string",
+            "Please pass a learning-track-id as a parameter (ex. &id=xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx) on the query string",
             status_code=400
         )
     
@@ -22,10 +22,17 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             "Unauthorized: Please pass a bearer token in the Authorization header",
             status_code=401
         )
+    try:
+      file_path = export_pdf(id, bearer_token)
+    except Exception as e:
+      return func.HttpResponse(
+          f"Error exporting pdf: {e}",
+          status_code=500
+      )
     
     try:
       return func.HttpResponse(
-          f"Id is {id}. bearer_token is {bearer_token}. This HTTP triggered function executed successfully.",
+          f"Id is {id}. bearer_token is {bearer_token}. Filepath is {file_path}. This HTTP triggered function executed successfully.",
           status_code=200
       )
     except Exception as e:
